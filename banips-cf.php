5701d9990568a550a261c6a7b028a8841b53a205
Jack Brown <andrew.carrsr@gmail.com>
2017-07-28 01:26:14 -0700

    Version 1.5 - Completely different & Functional
    This version is completely different. I wrote it within an hour or so. It's also a bit stylish depending on your taste. Definitely better then no style.It's functional as well. (The last version wasn't as-is functional)

--- banips-cf.php
+++ banips-cf.php
@@ -1,139 +1,163 @@
 <?php
-require_once(cf-api.php");
-$error = 0;
-$ips = "";
+require_once(__dir__ . "/_required/cf-api.php");
 
-function loadlogs($echos){
-	global $ips,$error;
-	$exploded = explode("\r\n", $_POST['logs']);
-	if($echos){
-		echo "\n<br/>List of IPs: \n<br/>";
+class banip{
+	public $err,$ip,$ips,$save,$file;
+	//public function cfban($ipaddr,$reason);
+	
+	public function __construct(){
+		//
+		$this->save = false;
+		$this->file = __dir__ . "/cfresultids.txt";
 	}
 	
-	foreach($exploded as &$explode)
-	{
-		$explodes = explode(' - ', $explode);
-		if(filter_var(trim($explodes[0]), FILTER_VALIDATE_IP) !== false){
-			if($echos){
-				echo "$explodes[0], ";
-			}
-			$ips .= $explodes[0].", ";
+	public function saveToFile($file,$content,$mode){
+		if (!isset($file)) {
+			return "Filename not defined.";
+			exit;
 		}
-		else{
-			$error+1;
-			if($echos){
-				if(!$explodes[0] == "" | !$explodes[0] == " "){
-					echo "\n<br/>Error! - ";
-					echo "\"".htmlentities($explodes[0])."\"";
+		if(!file_exists($file) && $mode == "a"){
+			$mode = "w";
+		}
+		if (!is_writable($file)) {
+			return "Could not write to {$file}.";
+			exit;
+		}
+		$fp = fopen($file, $mode);
+		if (!is_resource($fp)) {
+			return "Could not open {$file} for writting.";
+			exit;
+		}
+		fwrite($fp, $content);
+		fclose($fp);
+	}
+	
+	public function processlogs($inputlog,$echo=false){ //$echo is unnecessary now. I'll remove it in the future
+		$exploded = explode("\r\n", $inputlog); // May need to be changed to \n depending on the system.
+		if($echo){
+			echo "List of IPs: \n<br/>";
+		}
+		
+		foreach($exploded as &$explode){
+			$explodes = explode(' - ', $explode);
+			if(filter_var(trim($explodes[0]), FILTER_VALIDATE_IP) !== false){
+				if($echo){
+					echo "$explodes[0], ";
+				}
+				
+				$ips .= $explodes[0].", ";
+			}
+			else{
+				$error+1;
+				if($echo){
+					if(trim($explodes[0]) == "" || trim($explodes[0]) == " "){ //There's probably a better way to detect if its an empty line
+					}
+					else{
+						echo "\n<br/>Error! (\"".htmlentities($explodes[0])."\")";
+					}
 				}
 			}
+			
+			if($echo && $error != 0){
+				echo "\n<br/>There are $error errors";
+			}
+			
+			$ips = substr($ips, 0, -2); //Remove the last ", " from the string
 		}
-		//If there isn't an error, it is added.
-		//If there is an error, it is'nt added.
 	}
 	
-	if($echos){
-		echo "\n<br/>There are $error errors";
+	public function ajaxprocessing(){
+		if(isset($_GET['ajax']) && $_GET['ajax'] == "true"){
+			if(isset($_POST['confirmation']) && $_POST['confirmation'] == "true"){
+				echo "<br/>The result IDs are:";
+				$ips = $_POST['ips'];
+				$exploded = explode(', ', $ips);
+				
+				foreach($exploded as $explosion){
+					$cfban = cfban($explosion, "BanIPs-CF (Manual) for very suspicious behavior");
+					if($this->save){
+						echo $this->saveToFile($this->file,"$explosion: ".$cfban."\r\n","a");
+					}
+					if($cfban == false){
+						echo "Returned False (Failed to ban)";
+					}
+					else{
+						echo $cfban;
+					}
+					echo ", ";
+					sleep(0.1);
+				}
+				exit;
+				die; // Have to do this so it only returns this data, and nothing else.
+			}
+		}
 	}
 	
-	$ips = substr($ips, 0, -2);
+	public function processban(){
+		$ip = htmlentities($_POST['ip']);
+		$cfban = cfban($ip,"Ban-CF-Script (Manually) for ".htmlentities($_POST['reason']));
+		if($this->save){
+			echo $this->saveToFile($this->file,"$ip: ".$cfban."\r\n","a");	
+		}
+		echo "Result: ".$cfban;
+		if($cfban == false){
+			echo "Returned False (Failed to ban)";
+		}
+	}
+	
+	public function processunban(){
+		if($this->save == false){
+			echo "You disabled saving to file; Cannot continue!";
+			exit;
+		}
+		
+		$result=preg_split('/'.preg_quote(htmlentities($_POST['uip']).": ").'/',$this->file);
+		if(count($result)>1){
+			$result_split=explode(' ',$result[1]);
+			$result_split2=explode("\r\n",$result_split[1]);
+			echo "<br/>ID: ".$result_split2[0];
+			$cfunban = cfunban($result_split2[0]);
+			echo "<br/> Result: ".$cfunban;
+			if($cfunban == false){
+				echo " Returned False (Failed to Unban, have you already unbanned this ID?)";
+			}
+		}
+	}
 }
 
-if(isset($_GET['ajax']) && $_GET['ajax'] == "true"){
-	//loadlogs(false);
-	
-	if(isset($_POST['confirmation']) && $_POST['confirmation'] == "true"){	
-		echo "\n<br/><br/>The Result IDs are: ";
-		$ips = $_POST['ips'];
-		$exploded2 = explode(', ', $ips);
-		foreach($exploded2 as $explosion){
-			$cfban = cfban($explosion,"BanIPs-CF (Manual) for Very suspicious behavior");
-			echo $_require->saveToFile(S_DIR."cfresultids.txt","$explosion: ".$cfban."\r\n","a");
-			echo $cfban;
-			echo ", ";
-			sleep(0.1);
-		}
-	}
-	else{
-		echo "Cannot continue.";
-	}
-	exit(); die();
-}
+$banip = new banip();
+$banip->ajaxprocessing();
 ?>
 <html>
 <head>
-<script>
-var ajax = {};
-ajax.x = function () {
-    if (typeof XMLHttpRequest !== 'undefined') {
-        return new XMLHttpRequest();
-    }
-    var versions = [
-        "MSXML2.XmlHttp.6.0",
-        "MSXML2.XmlHttp.5.0",
-        "MSXML2.XmlHttp.4.0",
-        "MSXML2.XmlHttp.3.0",
-        "MSXML2.XmlHttp.2.0",
-        "Microsoft.XmlHttp"
-    ];
-
-    var xhr;
-    for (var i = 0; i < versions.length; i++) {
-        try {
-            xhr = new ActiveXObject(versions[i]);
-            break;
-        } catch (e) {
-        }
-    }
-    return xhr;
-};
-
-ajax.send = function (url, callback, method, data, async) {
-    if (async === undefined) {
-        async = true;
-    }
-    var x = ajax.x();
-    x.open(method, url, async);
-    x.onreadystatechange = function () {
-        if (x.readyState == 4) {
-			var serverResponse = x.responseText;
-            callback(x.responseText) 
-        }
-		/*x.onload = function (e) {
-    results.innerHTML = e.target.response.message;
-};*/
-    };
-    if (method == 'POST') {
-        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    }
-    x.send(data)
-};
-
-ajax.get = function (url, data, callback, async) {
-    var query = [];
-    for (var key in data) {
-        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-    }
-    ajax.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, async)
-};
-
-ajax.post = function (url, data, callback, async) {
-    var query = [];
-    for (var key in data) {
-        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-    }
-    ajax.send(url, callback, 'POST', query.join('&'), async)
-};
-</script>
+<title>The BanHammer</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://bootswatch.com/darkly/bootstrap.min.css">
 <style>
+footer {
+    padding: 1em;
+    color: white;
+    /*background-color: #222222;*/
+	text-align: center;
+	width: 100%;
+	bottom: 0;
+    position: fixed;
+}
+footer a{
+	color: white;
+}
+.footerdiv{
+	background: rgb(70, 69, 69, 0.5); /*#464545*/
+	border: 1px solid #222222;
+	text-shadow: 2px 2px #222222;
+}
+/**/
 .loader {
-  border: 16px solid #f3f3f3;
+  border: 16px solid #464545;
+  border-top: 16px solid #375a7f;
   border-radius: 50%;
-  border-top: 16px solid blue;
-  border-bottom: 16px solid blue;
   width: 120px;
   height: 120px;
-  -webkit-animation: spin 2s linear infinite;
   animation: spin 2s linear infinite;
   display: none;
 }
@@ -174,86 +198,98 @@ ajax.post = function (url, data, callback, async) {
 
 </style>
 </head>
-<body>
+<body> <br/><br/><br/>
 	<div id="left">
 		<form action="https://null.org.uk/banips-cf.php" method="POST">
-		Unban IP: <input type="text" name="uip" />
-		<button type="submit">GO!</button>
+			<div class="form-group">
+				<label class="control-label" for="focusedInput">Unban IP:</label>
+				<input class="form-control" id="focusedInput" name="uip" value="Enter IP here" type="text" style="width:70%;">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="submit">Go!</button>
+				</span>
+			</div>
 		</form>
 		<br/>
 	</div>
 	
 	<div id="right">
 		<form action="https://null.org.uk/banips-cf.php" method="POST">
-		IP: <input type="text" name="ip" /> <br/>
-		Reason: <input type="text" name="reason" />
-		<button type="submit">GO!</button>
+			<div class="form-group">
+				<label class="control-label" for="inputSmall">Ban IP:</label>
+				<input class="form-control input-sm" id="inputSmall" name="ip" value="Enter IP here" type="text" style="width:70%;"> <br/>
+				<input class="form-control input-sm" id="inputSmall" name="reason" value="Enter Reason here" type="text" style="width:70%;">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="submit">Go!</button>
+				</span>
+			</div>
 		</form>
 		<br/>
 	</div>
 	
 	<div id="center">
 	<form action="https://null.org.uk/banips-cf.php" method="POST" id="usrform">
-	NGINX logs: <textarea name="logs" form="usrform">Enter text here...</textarea> 
-	<button type="submit">GO!</button>
+		<div class="form-group">
+			<div class="input-group">
+				<label class="control-label">Input NGINX Logs</label>
+				<textarea class="form-control" name="logs" form="usrform">Enter text here...</textarea>
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="submit">Go!</button>
+				</span>
+			</div>
+		</div>
 	</form>
+	
 	<br/>
 	<center>
 	<?php
 	if(isset($_POST['ip']) && $_POST['ip'] != "" && isset($_POST['reason']) && $_POST['reason'] != ""){
-		//echo cfban(htmlentities($_POST['ip']));
-		$cfban = cfban($explosion,"Ban-CF-Script (Manually) for ".htmlentities($_POST['reason']));
-		echo $_require->saveToFile(S_DIR."cfresultids.txt","$explosion: ".$cfban."\r\n","a");	
-		echo $cfban;
-	}
-	else{
-	//	echo "\n<br/>No IP set";
+		$banip->processban();
 	}
 	
 	if(isset($_POST['uip']) && $_POST['uip'] != ""){
-		echo "aye captin. <br/>\n";
-		$result=preg_split('/'.preg_quote($_POST['uip'].": ").'/',$_require->getFile(S_DIR."cfresultids.txt"));
-		//$result = explode("\r\n", $result);
-		if(count($result)>1){
-			$result_split=explode(' ',$result[1]);
-			$result_split2=explode("\r\n",$result_split[1]);
-			echo(cfunban($result_split2[0]));
-		}
+		$banip->processunban();
 	}
 	
 	if(isset($_POST['logs']) && $_POST['logs'] != ""){
-		echo "<br/><br/>";
-		loadlogs(true);
+		$banip->processlogs($_POST['logs'], true);
 		
 		echo "\n<br/>Do you wish to continue?<br/>";
 		?>
-		<button id="retrieve" data-url="/">Confirm!</button>
+		<button id="retrieve" class="btn btn-default" data-url="/">Confirm!</button>
 		<br/><br/>
 		<div class="loader" id="loader"></div>
 		<p id="results"></p>
 		<script nonce="bpquxr9yrckd9sg">
 		function pushresult(e){
 			results.innerHTML = e;
-			document.getElementById('loader').setAttribute("style", "display:none;");
+			//document.getElementById('loader').setAttribute("style", "display:none;");
 		}
-		function yourFunction(){
-			retrieve.addEventListener('click', function (e) {
-				document.getElementById('loader').setAttribute("style", "display:block;");
-				ajax.post('/banips-cf.php?ajax=true', {confirmation: 'true'<?php
-				/*if(isset($_POST)){
-					foreach($_POST as $k => $v){
-						echo ",$k: \"".htmlentities($v)."\"";
-					}
-				}*/ echo ", \nips: '".$ips."'";
-				?>}, pushresult);
+		
+		$(document).ready(function(){
+			$(document).ajaxStart(function(){
+				$("#loader").css("display", "block");
 			});
-		}
-
-		yourFunction();
+			$(document).ajaxComplete(function(){
+				$("#loader").css("display", "none");
+			});
+			$("#retrieve").click(function(){
+				//document.getElementById('loader').setAttribute("style", "display:block;");
+				$.ajax({
+					type: "POST",
+					url: "/banips-cf.php?ajax=true",
+					data:{confirmation: 'true'<?php echo ", \nips: '".$ips."'";?>},
+					success: pushresult
+				});
+			});
+		});
 		</script>
 		<?php
 	}?>
 	</center>
+	<br/><br/><br/>
 	</div>
+	
+	<br/><p style="visibility: hidden;">Hidden-Content</p><br/> <!-- The footer could cover up content if I don't do this -->
+	<footer><div class="footerdiv">Created by <a href="https://github.com/andrewsalmon">Andrew Salmon</a><br/></div></footer>
 </body>
 </html>
